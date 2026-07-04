946695822bdfe0348ceeabe08907471a43907f6f
votantai3070 <votantai.07092002@gmail.com>
2026-07-04 15:14:29 +0700

    fix again

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,9 @@
 import LoginForm from "../../components/auth/LoginForm";
 
-// export default function LoginPage() {
-//   return (
-//     <>
-//       <LoginForm />
-//     </>
-//   );
-// }
-
 export default function LoginPage() {
-  return <div>LOGIN OK</div>;
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
 }
